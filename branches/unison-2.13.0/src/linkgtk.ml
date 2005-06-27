912a05790d5d2d66d8257dd17d81808c7cd1c150
bcpierce <bcpierce@4bce34ff-96ee-0310-b826-95abcfff6e26>
2005-06-27 21:05:03 +0000

    Tidying for next attempt

--- branches/unison-2.13.0/src/linkgtk.ml
+++
@@ -1,5 +0,0 @@
-(* $I1: Unison file synchronizer: src/linkgtk.ml $ *)
-(* $I2: Last modified by vouillon on Wed, 26 Apr 2000 20:04:29 -0400 $ *)
-(* $I3: Copyright 1999-2004 (see COPYING for details) $ *)
-
-module TopLevel = Main.Body(Uigtk.Body)
