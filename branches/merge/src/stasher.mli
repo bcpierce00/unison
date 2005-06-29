f8b15ec060f1895b154695ced0e6349768799ba1
lescuyer <lescuyer@4bce34ff-96ee-0310-b826-95abcfff6e26>
2005-06-29 16:10:57 +0000

    added Stasher.ml and Stasher.mli to the repository (they were missing yesterday), some tidying has been done

---
+++ branches/merge/src/stasher.mli
@@ -0,0 +1,14 @@
+(* $I1: Unison file synchronizer: src/stasher.mli $ *)
+(* $I2: Last modified by lescuyer on *)
+(* $I3: Copyright 1999-2005 (see COPYING for details) $ *)
+
+(* This module maintains backups for general purpose and *)
+(* as archives for mergeable files. *)
+
+(* Archives for merge purposes *)
+val stashCurrentVersion: Path.local -> unit
+val getRecentVersion: Fspath.t -> Path.local -> Os.fullfingerprint -> Fspath.t option
+
+(* Backups *)
+val initBackups: unit -> unit
+val removeAndBackupAsAppropriate: Fspath.t -> Path.local -> Fspath.t -> Path.local -> unit
