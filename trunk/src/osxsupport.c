9ca4cbe8d4ff0d36fe57dea1e5a83d8425edf300
bcpierce <bcpierce@4bce34ff-96ee-0310-b826-95abcfff6e26>
2005-09-26 19:46:19 +0000

    * Incorporated old patch to set read-only file to R/W on OSX before   attempting to change other attributes, per discussion on   unison-hackers list.

--- trunk/src/osxsupport.c
+++ trunk/src/osxsupport.c
@@ -6,6 +6,8 @@
 #include <caml/alloc.h>
 #include <caml/memory.h>
 #ifdef __APPLE__
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/attr.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -99,6 +101,21 @@ CAMLprim value setFileInfos (value path, value fInfo) {
 
   retcode = setattrlist(String_val (path), &attrList, attrBuf.finderInfo,
                         sizeof attrBuf.finderInfo, options);
+
+  if (retcode == -1 && errno == EACCES) {
+    /* Unlike with normal Unix attributes, we cannot set OS X attributes 
+       if file is read-only.  Try making it writable temporarily. */
+    struct stat st;
+    int r = stat(String_val(path), &st);
+    if (r == -1) uerror("setattrlist", path);
+    r = chmod(String_val(path), st.st_mode | S_IWUSR);
+    if (r == -1) uerror("setattrlist", path);
+    /* Try again */
+    retcode = setattrlist(String_val (path), &attrList, attrBuf.finderInfo,
+                          sizeof attrBuf.finderInfo, options);
+    /* Whether or not that worked, we should try to set the mode back. */
+    chmod(String_val(path), st.st_mode);
+  }
 
   if (retcode == -1) uerror("setattrlist", path);
 
