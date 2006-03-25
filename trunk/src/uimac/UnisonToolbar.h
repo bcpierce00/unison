71ded26cbad3471342002e5de33e46022830a80f
ben <ben@4bce34ff-96ee-0310-b826-95abcfff6e26>
2006-03-25 22:34:40 +0000

    Add toolbar-related files missing from previous commit

---
+++ trunk/src/uimac/UnisonToolbar.h
@@ -0,0 +1,27 @@
+//
+//  UnisonToolbar.h
+//  
+//  Extended NSToolbar with several views
+//
+//  Created by Ben Willmore on Sun March 12 2006.
+//  Copyright (c) 2006, licensed under GNU GPL.
+//
+
+#import <AppKit/AppKit.h>
+
+@class ReconTableView, MyController;
+
+@interface UnisonToolbar : NSToolbar
+{
+	ReconTableView*  tableView;
+	MyController*    myController;
+}
+
+- initWithIdentifier:(NSString *) identifier :(MyController *) aController :(ReconTableView *) aTableView;
+- (NSToolbarItem *) toolbar: (NSToolbar *)toolbar itemForItemIdentifier: (NSString *) itemIdent willBeInsertedIntoToolbar:(BOOL) willBeInserted;
+- (NSArray *) itemIdentifiersForView: (NSString *) whichView;
+- (NSArray *) toolbarDefaultItemIdentifiers: (NSToolbar *) toolbar;
+- (NSArray *) toolbarAllowedItemIdentifiers: (NSToolbar *) toolbar;
+- (void) setView: (NSString *) whichView;
+
+@end
