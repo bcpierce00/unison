09301a401358d8533adbe924a082cd08f990743e
Benjamin C. Pierce <bcpierce@cis.upenn.edu>
2007-10-04 03:18:34 +0000

    * Applying a patch from Karl M to make the Restart button reload the   profile in the uigtk2 UI.

--- src/uimacnew/ProgressCell.m
+++ src/uimacnew/ProgressCell.m
@@ -1,27 +1,8 @@
 /******************************************************************************
- * Note -- loosely based on TorrentCell from Transmission (mostly just the .png
- * files.  Their original copyright is included below...
+ * Copyright 2007 (see file COPYING for more information)
  *
- *
- * Copyright (c) 2006-2007 Transmission authors and contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ * Loosely based on TorrentCell from Transmission (.png files are from 
+ * the original).  
  *****************************************************************************/
 
 #import "ProgressCell.h"
