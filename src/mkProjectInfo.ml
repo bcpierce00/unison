7297ca7309438c43567b1ab14b54642d9b4e0d44
Benjamin C. Pierce <bcpierce@cis.upenn.edu>
2008-12-01 14:00:40 +0000

    * Bumped version number to reflect newly added preference

--- src/mkProjectInfo.ml
+++ src/mkProjectInfo.ml
@@ -5,8 +5,8 @@
 
 let projectName = "unison"
 let majorVersion = 2
-let minorVersion = 31
-let pointVersionOrigin = 301 (* Revision that corresponds to point version 0 *)
+let minorVersion = 32
+let pointVersionOrigin = 313 (* Revision that corresponds to point version 0 *)
 
 (* Documentation:
    This is a program to construct a version of the form Major.Minor.Point,
@@ -49,6 +49,7 @@ let pointVersion = revision - pointVersionOrigin;;
 Printf.printf "MAJORVERSION=%d.%d\n" majorVersion minorVersion;;
 Printf.printf "VERSION=%d.%d.%d\n" majorVersion minorVersion pointVersion;;
 Printf.printf "NAME=%s\n" projectName;;
+
 
 
 
