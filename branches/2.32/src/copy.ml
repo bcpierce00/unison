a762eef1664803af31091dd62645934eb424ac7f
vouillon <vouillon@4bce34ff-96ee-0310-b826-95abcfff6e26>
2010-01-22 10:26:14 +0000

    Fixes to the stable release: * Fixed bug which made Unison ignore finder information and resource   fork when compiled to 64bit on Mac OSX. * IPV6, socket mode: properly deal with Unix errors, so that Unison   correctly falls back to IPV4 if the kernel does not support IPV6 * copyprog: use better rsync options

--- branches/2.32/src/copy.ml
+++ branches/2.32/src/copy.ml
@@ -506,7 +506,7 @@ let transferFile
 (****)
 
 let copyprog =
-  Prefs.createString "copyprog" "rsync --inplace --compress"
+  Prefs.createString "copyprog" "rsync --partial --inplace --compress"
     "!external program for copying large files"
     ("A string giving the name of an "
      ^ "external program that can be used to copy large files efficiently  "
@@ -515,7 +515,7 @@ let copyprog =
      ^ "options---most users should not need to change it.")
 
 let copyprogrest =
-  Prefs.createString "copyprogrest" "rsync --partial --inplace --compress"
+  Prefs.createString "copyprogrest" "rsync --partial --append-verify --compress"
     "!variant of copyprog for resuming partial transfers"
     ("A variant of {\\tt copyprog} that names an external program "
      ^ "that should be used to continue the transfer of a large file "
