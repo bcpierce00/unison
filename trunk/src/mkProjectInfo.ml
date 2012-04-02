18db012be4ba2a13b753178193ea717083112f81
bcpierce <bcpierce@4bce34ff-96ee-0310-b826-95abcfff6e26>
2012-04-02 15:56:58 +0000

    * Bit more preparation for beta release

--- trunk/src/mkProjectInfo.ml
+++ trunk/src/mkProjectInfo.ml
@@ -6,7 +6,7 @@
 let projectName = "unison"
 let majorVersion = 2
 let minorVersion = 45
-let pointVersionOrigin = 485 (* Revision that corresponds to point version 0 *)
+let pointVersionOrigin = 487 (* Revision that corresponds to point version 0 *)
 
 (* Documentation:
    This is a program to construct a version of the form Major.Minor.Point,
@@ -50,6 +50,7 @@ let pointVersion =
 Printf.printf "MAJORVERSION=%d.%d\n" majorVersion minorVersion;;
 Printf.printf "VERSION=%d.%d.%d\n" majorVersion minorVersion pointVersion;;
 Printf.printf "NAME=%s\n" projectName;;
+
 
 
 
