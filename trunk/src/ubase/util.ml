61f461d1de38c025a59f451df9a9ab7ba27afb2e
bcpierce <bcpierce@4bce34ff-96ee-0310-b826-95abcfff6e26>
2014-09-28 16:48:05 +0000

    * Incorporate a small patch from Christopher Zimmermann to replace the   Uprintf module (which doesn't work with OCaml 4.02) with equivalent   functionality from the standard library.

--- trunk/src/ubase/util.ml
+++ trunk/src/ubase/util.ml
@@ -62,7 +62,8 @@ let set_infos s =
   if s <> !infos then begin clear_infos (); infos := s; show_infos () end
 
 let msg f =
-  clear_infos (); Uprintf.eprintf (fun () -> flush stderr; show_infos ()) f
+  clear_infos ();
+  Printf.kfprintf (fun _ -> flush stderr; show_infos ()) stderr f
 
 let msg : ('a, out_channel, unit) format -> 'a = msg
 
