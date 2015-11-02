1b850c8fb5fd6ef78750933bbac655bd08d047c2
bcpierce <bcpierce@4bce34ff-96ee-0310-b826-95abcfff6e26>
2015-11-02 01:35:17 +0000

    * Remove colon from names of files created by the -copyonconflict   flag, to avoid unhappiness with some filesystems.

--- trunk/src/files.ml
+++ trunk/src/files.ml
@@ -96,7 +96,7 @@ let finishCopy copyInfo =
       let rec copyPath n =
         let p =
           Path.addToFinalName path
-            (Format.sprintf " (copy: conflict%s on %04d-%02d-%02d)"
+            (Format.sprintf " (conflict%s on %04d-%02d-%02d)"
                (if n = 0 then "" else " #" ^ string_of_int n)
                (tm.Unix.tm_year + 1900) (tm.Unix.tm_mon + 1) tm.Unix.tm_mday)
         in
