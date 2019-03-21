29fa058c3127f3b47c347dcaa4a94f4c0e888308
Jaap Boender <jaapb@kerguelen.org>
2019-03-21 12:26:51 +0000

    Compatibility with OCaml 4.08

--- src/recon.ml
+++ src/recon.ml
@@ -651,8 +651,8 @@ let rec reconcile
 
 (* Sorts the paths so that they will be displayed in order                   *)
 let sortPaths pathUpdatesList =
-  Sort.list
-    (fun (p1, _) (p2, _) -> Path.compare p1 p2 <= 0)
+  List.sort
+    Path.compare
     pathUpdatesList
 
 let rec enterPath p1 p2 t =
