090746f0591ef898f59e8485246ad7afa55131c8
Benjamin Pierce <bcpierce00t@gmail.com>
2019-11-29 15:50:05 -0500

    Update copyright dates

--- src/fingerprint.mli
+++ src/fingerprint.mli
@@ -1,5 +1,5 @@
 (* Unison file synchronizer: src/fingerprint.mli *)
-(* Copyright 1999-2018, Benjamin C. Pierce (see COPYING for details) *)
+(* Copyright 1999-2020, Benjamin C. Pierce (see COPYING for details) *)
 
 type t
 
