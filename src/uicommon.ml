bcpierce00/unison

commit fa93acdf451aabbf50ccb7a29b5a96e8a8e5d071

Make roots parsing and installing more resilient...
... and actually report nice errors to the user, rather than just fail
with an uncaught exception and a backtrace. While the bulk of this
change is in the GUI, the resiliency and error reporting applies to CLI
as well.

Hint:
  the following git options may help when reviewing

  --color-moved=blocks --color-moved-ws=ignore-space-change

src/uicommon.ml
@@ -634,6 +634,10 @@ let initPrefs ~profileName ~displayWaitMessage ~promptForRoots
               ?(prepDebug = fun () -> ()) ~termInteract () =
   (* Restore prefs to their default values, if necessary *)
   if not !firstTime then Prefs.resetToDefaults();
+  (* Clear out any roots left from a previous profile. They can't remain
+     hanging around if [initPrefs] for the new profile receives an exception
+     before fully completing. *)
+  Globals.uninstallRoots ();
   Globals.setRawRoots !cmdLineRawRoots;
 
   (* Tell the preferences module the name of the profile *)
@@ -711,13 +715,20 @@ let initPrefs ~profileName ~displayWaitMessage ~promptForRoots
         end
   end;
 
+  (* Parse the roots to validate them *)
+  let parsedRoots =
+    try Safelist.map Clroot.parseRoot (Globals.rawRoots ()) with
+    | Invalid_argument s | Util.Fatal s | Prefs.IllegalValue s ->
+        raise (Util.Fatal ("There's a problem with one of the roots:\n" ^ s))
+  in
+
   (* Check to be sure that there is at most one remote root *)
   let numRemote =
     Safelist.fold_left
-      (fun n r -> match Clroot.parseRoot r with
+      (fun n (r : Clroot.clroot) -> match r with
         ConnectLocal _ -> n | ConnectByShell _ | ConnectBySocket _ -> n+1)
       0
-      (Globals.rawRoots ()) in
+      parsedRoots in
       if numRemote > 1 then
         raise(Util.Fatal "cannot synchronize more than one remote root");
 

src/clroot.ml
@@ -89,7 +89,7 @@ let getProtocolSlashSlash s =
                   (Printf.sprintf "protocol unison has been deprecated, use file, ssh, rsh, or socket instead" ))
       | _ ->
           raise(Invalid_argument
-                  (Printf.sprintf "unrecognized protocol %s" protocolName)) in
+                  (Printf.sprintf "\"%s\": unrecognized protocol %s" s protocolName)) in
     Some(protocol,remainder)
   else if Str.string_match slashSlashRegexp s 0
   then Some(File,String.sub s 2 (String.length s - 2))
@@ -100,7 +100,7 @@ let getProtocolSlashSlash s =
       "file:" | "ssh:" | "rsh:" | "socket:" ->
         raise(Util.Fatal
                 (Printf.sprintf
-                   "ill-formed root specification %s (%s must be followed by //)"
+                   "ill-formed root specification \"%s\" (%s must be followed by //)"
                    s matched))
     | _ -> None
   else None
@@ -150,6 +150,7 @@ let getPort s =
    and path is guaranteed to be non-empty
 *)
 let parseUri s =
+  let s = Util.trimWhitespace s in
   match getProtocolSlashSlash s with
     None ->
       (File,None,None,None,Some s)
@@ -199,7 +200,7 @@ let cannotAbbrevFileRx = Rx.rx "(file:|ssh:|rsh:|socket:).*"
 let networkNameRx = Rx.rx "//.*"
 (* Main external printing function *)
 let clroot2string = function
-  ConnectLocal None -> "."
+| ConnectLocal None | ConnectLocal (Some "") -> "."
 | ConnectLocal(Some s) ->
     if Rx.match_string cannotAbbrevFileRx s
     then if Rx.match_string networkNameRx s
@@ -243,7 +244,7 @@ let fixHost = function
 let parseRoot string =
   let illegal2 s = raise(Prefs.IllegalValue
                            (Printf.sprintf
-                              "%s: %s" string s)) in
+                              "\"%s\": %s" string s)) in
   let (protocol,user,host,port,path) = parseUri string in
   let clroot =
     match protocol,user,host,port with

src/globals.ml
@@ -51,8 +51,11 @@ let rawRootPair () =
 
 let theroots = ref []
 
+let uninstallRoots () = theroots := []
+
 open Lwt
 let installRoots termInteract =
+  let () = uninstallRoots () in (* Clear out potential old roots *)
   let roots = rawRoots () in
   if Safelist.length roots <> 2 then
     raise (Util.Fatal (Printf.sprintf
@@ -66,6 +69,14 @@ let installRoots termInteract =
        cont >>= (fun l ->
        return (r' :: l))))
     roots (return []) >>= (fun roots' ->
+  let () = match roots' with
+           | [r1; r2] when r1 = r2 ->
+               raise (Util.Fatal (Printf.sprintf
+                   ("That's no good, the roots appear to be the same! Here's "
+                 ^^ "what I found:\nFirst root: %s\nSecond root: %s")
+                   (Common.root2string r1) (Common.root2string r2)))
+           | _ -> ()
+  in
   theroots := roots';
   Negotiate.features (Common.sortRoots roots') >>=
   return)
@@ -73,6 +84,7 @@ let installRoots termInteract =
 (* Alternate interface, should replace old interface eventually *)
 let installRoots2 () =
   debug (fun () -> Util.msg "Installing roots...");
+  let () = uninstallRoots () in (* Clear out potential old roots *)
   let roots = rawRoots () in
   theroots :=
     Safelist.map Remote.canonize ((Safelist.map Clroot.parseRoot) roots);

src/globals.mli
@@ -16,6 +16,9 @@ val installRoots : (string -> string -> string) option -> unit Lwt.t
 (* An alternate method (under development?) *)
 val installRoots2 : unit -> unit
 
+(* Clear previously installed roots; typically used when switching profiles  *)
+val uninstallRoots : unit -> unit
+
 (* The roots of the synchronization (with names canonized, but in the same   *)
 (* order as the user gave them)                                              *)
 val roots : unit -> Common.root * Common.root

src/uigtk2.ml
@@ -736,7 +736,7 @@ let getFirstRoot () =
   let f3 = t#action_area in
   let result = ref None in
   let contCommand() =
-    result := Some(fileE#text);
+    result := Some (Util.trimWhitespace fileE#text);
     t#destroy () in
   let cancelButton = GButton.button ~stock:`CANCEL ~packing:f3#add () in
   ignore (cancelButton#connect#clicked
@@ -833,10 +833,10 @@ let getSecondRoot () =
   ignore (socketB#connect#clicked ~callback:(fun () -> protoState(`SOCKET)));
   localState();
   let getRoot() =
-    let file = fileE#text in
-    let user = userE#text in
-    let host = hostE#text in
-    let port = portE#text in
+    let file = Util.trimWhitespace fileE#text in
+    let user = Util.trimWhitespace userE#text in
+    let host = Util.trimWhitespace hostE#text in
+    let port = Util.trimWhitespace portE#text in
     match !varLocalRemote with
       `Local ->
         Clroot.clroot2string(Clroot.ConnectLocal(Some file))
@@ -863,9 +863,9 @@ let getSecondRoot () =
         okBox ~parent:t ~title:"Error" ~typ:`ERROR ~message:"Please enter a port"
       else okBox ~parent:t ~title:"Error" ~typ:`ERROR
           ~message:"The port you specify must be an integer"
-    | _ ->
+    | Util.Transient s | Util.Fatal s | Invalid_argument s | Prefs.IllegalValue s ->
       okBox ~parent:t ~title:"Error" ~typ:`ERROR
-        ~message:"Something's wrong with the values you entered, try again" in
+        ~message:("Something's wrong with the values you entered, try again.\n" ^ s) in
   let f3 = t#action_area in
   let cancelButton =
     GButton.button ~stock:`CANCEL ~packing:f3#add () in
@@ -1525,17 +1525,54 @@ let createProfile parent =
        then
          assistant#set_current_page (p + 1)));
 
+  let conclusionOk = "You have now finished filling in the profile.\n\n\
+             Click \"Apply\" to create it."
+  and conclusionFail = "There was an error when preparing the profile.\n\n\
+             Click \"Back\" to review what you entered." in
   let conclusion =
     GMisc.label
       ~xpad:12 ~ypad:12
-      ~text:"You have now finished filling in the profile.\n\n\
-             Click \"Apply\" to create it."
+      ~text:conclusionOk
     () in
-  ignore
+  let conclusionp =
     (assistant#append_page
        ~title:"Done" ~complete:true
        ~page_type:`CONFIRM
-       conclusion#as_widget);
+       conclusion#as_widget) in
+
+  let makeRemoteRoot () =
+    let secondDir = Util.trimWhitespace (React.state secondDir) in
+    let host = Util.trimWhitespace (React.state host) in
+    let user = match React.state user with "" -> None | u -> Some (Util.trimWhitespace u) in
+    let secondRoot =
+      match React.state kind with
+        `Local  -> Clroot.ConnectLocal (Some secondDir)
+      | `SSH    -> Clroot.ConnectByShell
+                     ("ssh", host, user, None, Some secondDir)
+      | `RSH    -> Clroot.ConnectByShell
+                     ("rsh", host, user, None, Some secondDir)
+      | `SOCKET -> Clroot.ConnectBySocket
+                     (host, React.state port, Some secondDir)
+    in
+    try
+      let root = Clroot.clroot2string (Clroot.fixHost secondRoot) in
+      ignore (Clroot.parseRoot root);
+      Some root
+    with
+    | Util.Transient s | Util.Fatal s | Invalid_argument s | Prefs.IllegalValue s ->
+        begin
+          okBox ~parent ~title:"Error" ~typ:`ERROR
+            ~message:("There was a problem with the remote root "
+              ^ "data you entered.\n\n" ^ s);
+          None
+        end
+  in
+  ignore (assistant#connect#prepare ~callback:(fun () ->
+    if assistant#current_page = conclusionp then
+    let ok = (React.state kind = `Local) || (makeRemoteRoot () <> None) in
+    let () = setPageComplete conclusion ok in
+    if ok then conclusion#set_text conclusionOk
+    else conclusion#set_text conclusionFail));
 
   let profileName = ref None in
   let saveProfile () =
@@ -1548,20 +1585,12 @@ let createProfile parent =
       let label = React.state label in
       if label <> "" then Printf.fprintf ch "label = %s\n" label;
       Printf.fprintf ch "root = %s\n" (React.state firstDir);
-      let secondDir = React.state secondDir in
-      let host = React.state host in
-      let user = match React.state user with "" -> None | u -> Some u in
       let secondRoot =
-        match React.state kind with
-          `Local  -> Clroot.ConnectLocal (Some secondDir)
-        | `SSH    -> Clroot.ConnectByShell
-                       ("ssh", host, user, None, Some secondDir)
-        | `RSH    -> Clroot.ConnectByShell
-                       ("rsh", host, user, None, Some secondDir)
-        | `SOCKET -> Clroot.ConnectBySocket
-                       (host, React.state port, Some secondDir)
+        match makeRemoteRoot () with
+        | None -> assert false (* We should never reach here due to validation above *)
+        | Some s -> s
       in
-      Printf.fprintf ch "root = %s\n" (Clroot.clroot2string (Clroot.fixHost secondRoot));
+      Printf.fprintf ch "root = %s\n" secondRoot;
       if React.state compress && React.state kind = `SSH then
         Printf.fprintf ch "sshargs = -C\n";
 (*

commit afb45ae741988ab2a6d2f85008b43fcc6eb62d85

Fix connection refresh after installRoots failure
Fix a bug introduced by commit fa93acdf451aabbf50ccb7a29b5a96e8a8e5d071

src/uicommon.ml
@@ -747,7 +747,7 @@ let initPrefs ~profileName ~displayWaitMessage ~promptForRoots
   firstTime := false
 
 let refreshConnection ~displayWaitMessage ~termInteract =
-  assert (Safelist.length (Globals.rootsList ()) > 1);
+  assert (Safelist.length (Globals.rawRoots ()) > 1);
   let numConn = ref 0 in
   Lwt_unix.run (Globals.allRootsIter
     (fun r -> if Remote.isRootConnected r then incr numConn; Lwt.return ()));