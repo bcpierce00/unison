bcpierce00/unison

commit 5d6ab3cb9921a476e73e42307d40addc1b86f30a

Make [Uicommon.repeat] a structured preference
Parse and validate [Uicommon.repeat] at preference parsing instead of
letting the code using the value interpret it freely and possibly error
out very late in the process.

Restructure the code in text UI to better align with this new preference
representation and make it easier to expand in future.

src/uicommon.ml
@@ -103,7 +103,23 @@ let contactingServerMsg () =
   Printf.sprintf "Unison %s: Contacting server..." Uutil.myVersion 
 
 let repeat =
-  Prefs.createString "repeat" ""
+  let parseRepeat s =
+    let parseTime ts =
+      try int_of_string ts with Failure _ ->
+        raise (Prefs.IllegalValue ("Value of 'repeat' preference ("
+          ^ s ^ ") should be either a number or 'watch'"))
+    in
+    match s with
+    | "" -> `NoRepeat
+    | "watch" -> `Watch
+    | _ -> `Interval (parseTime s)
+  in
+  let externRepeat = function
+    | `NoRepeat -> ""
+    | `Watch -> "watch"
+    | `Interval i -> string_of_int i
+  in
+  Prefs.create "repeat" `NoRepeat
     ~category:(`Advanced `Syncprocess_CLI)
     "synchronize repeatedly (text interface only)"
     ("Setting this preference causes the text-mode interface to synchronize "
@@ -112,7 +128,10 @@ let repeat =
      ^ "beginning again. When the argument is \\verb|watch|, Unison relies on "
      ^ "an external file monitoring process to synchronize whenever a change "
      ^ "happens.")
-let repeatWatcher () = Prefs.read repeat = "watch"
+    (fun _ -> parseRepeat)
+    (fun r -> [externRepeat r])
+    Umarshal.(sum1 string externRepeat parseRepeat)
+let repeatWatcher () = Prefs.read repeat = `Watch
 
 let retry =
   Prefs.createInt "retry" 0

src/uicommon.mli
@@ -34,7 +34,7 @@ val contactingServerMsg : unit -> string
 val profileLabel : string Prefs.t
 
 (* User preference: Synchronize repeatedly *)
-val repeat : string Prefs.t
+val repeat : [ `NoRepeat | `Interval of int | `Watch ] Prefs.t
 
 (* User preference: Try failing paths N times *)
 val retry : int Prefs.t

src/uitext.ml
@@ -1051,7 +1051,7 @@ let rec interactAndPropagateChanges prevItemList reconItemList
     (* JV (5/09): Don't save the archive in repeat mode as it has some
        costs and its unlikely there is much change to the archives in
        this mode. *)
-    if !Update.foundArchives && Prefs.read Uicommon.repeat = "" then
+    if !Update.foundArchives && Prefs.read Uicommon.repeat = `NoRepeat then
       Update.commitUpdates ();
     display "No updates to propagate\n";
     if skipped > 0 then begin
@@ -1195,7 +1195,7 @@ let synchronizeOnce ?wantWatcher pathsOpt =
 
   if not !Update.foundArchives then Update.commitUpdates ();
   if reconItemList = [] then begin
-    if !Update.foundArchives && Prefs.read Uicommon.repeat = "" then
+    if !Update.foundArchives && Prefs.read Uicommon.repeat = `NoRepeat then
       Update.commitUpdates ();
     (if anyEqualUpdates then
       Trace.status ("Nothing to do: replicas have been changed only "
@@ -1285,19 +1285,7 @@ let synchronizeUntilNoFailures repeatMode =
     end in
   loop (Prefs.read Uicommon.retry) None
 
-let rec synchronizeUntilDone () =
-  let repeatinterval =
-    if Prefs.read Uicommon.repeat = "" then -1 else
-    try int_of_string (Prefs.read Uicommon.repeat)
-    with Failure _ ->
-      (* If the 'repeat' pref is not a valid number, switch modes... *)
-      if Prefs.read Uicommon.repeat = "watch" then
-        synchronizePathsFromFilesystemWatcher()
-      else
-        raise (Util.Fatal ("Value of 'repeat' preference ("
-                           ^Prefs.read Uicommon.repeat
-                           ^") should be either a number or 'watch'\n")) in
-
+let rec synchronizeUntilDone repeatinterval =
   let exitStatus = synchronizeUntilNoFailures(repeatinterval >= 0) in
   if repeatinterval < 0 then
     exitStatus
@@ -1306,8 +1294,14 @@ let rec synchronizeUntilDone () =
     Trace.status (Printf.sprintf
        "\nSleeping for %d seconds...\n" repeatinterval);
     Unix.sleep repeatinterval;
-    synchronizeUntilDone ()
+    synchronizeUntilDone repeatinterval
   end
+
+let synchronizeUntilDone () =
+  match Prefs.read Uicommon.repeat with
+  | `Watch -> synchronizePathsFromFilesystemWatcher ()
+  | `Interval i -> synchronizeUntilDone i
+  | `NoRepeat -> synchronizeUntilDone (-1)
 
 (* ----------------- Startup ---------------- *)
 
@@ -1495,7 +1489,7 @@ let rec start interface =
       Prefs.set dumbtty true;
       Trace.sendLogMsgsToStderr := false;
     end;
-    if Prefs.read Uicommon.repeat <> "" then begin
+    if Prefs.read Uicommon.repeat <> `NoRepeat then begin
       Prefs.set Globals.batch true;
     end;
     setColorPreference ();
@@ -1528,7 +1522,7 @@ let rec start interface =
       (* If any other bad thing happened and the -repeat preference is
          set, then restart *)
       handleException e;
-      if Prefs.read Uicommon.repeat = ""
+      if Prefs.read Uicommon.repeat = `NoRepeat
           || Prefs.read Uicommon.runtests then
         exit Uicommon.fatalExit;
 

commit 640fedad96570b4297e12efc653c485f7862d3b9

Fix a potential future issue with repeat pref
Commit 5d6ab3cb9921a476e73e42307d40addc1b86f30a introduced a potential
issue with backwards compatibility. The issue could be triggered by a
future change in the syntax of the 'repeat' preference.

Due to legacy reasons, the value of the 'repeat' preference is sent to
the server, even though it is not used by the server. This means that
extending the set of valid values for the preference could break older
servers. This was not a problem before commit
5d6ab3cb9921a476e73e42307d40addc1b86f30a because the value of this
preference, even though sent to it, was never parsed by the server.
Since that commit, the value is however always parsed, and could cause
the server to raise a fatal exception if it fails to parse the new
syntax.

Fix the issue by capturing the error in the type instead of directly
raising it as an exception.

src/uicommon.ml
@@ -109,13 +109,16 @@ let repeat =
         raise (Prefs.IllegalValue ("Value of 'repeat' preference ("
           ^ s ^ ") should be either a number or 'watch'"))
     in
-    match s with
-    | "" -> `NoRepeat
-    | "watch" -> `Watch
-    | _ -> `Interval (parseTime s)
+    try
+      match s with
+      | "" -> `NoRepeat
+      | "watch" -> `Watch
+      | _ -> `Interval (parseTime s)
+    with
+    | Prefs.IllegalValue _ as e -> `Invalid (s, e)
   in
   let externRepeat = function
-    | `NoRepeat -> ""
+    | `NoRepeat | `Invalid _ -> ""
     | `Watch -> "watch"
     | `Interval i -> string_of_int i
   in

src/uicommon.mli
@@ -34,7 +34,8 @@ val contactingServerMsg : unit -> string
 val profileLabel : string Prefs.t
 
 (* User preference: Synchronize repeatedly *)
-val repeat : [ `NoRepeat | `Interval of int | `Watch ] Prefs.t
+val repeat : [ `NoRepeat | `Interval of int | `Watch
+  | `Invalid of string * exn ] Prefs.t
 
 (* User preference: Try failing paths N times *)
 val retry : int Prefs.t

src/uitext.ml
@@ -1376,6 +1376,7 @@ let synchronizeUntilDone () =
   | `Watch -> synchronizePathsFromFilesystemWatcher ()
   | `Interval i -> synchronizeUntilDone i
   | `NoRepeat -> synchronizeUntilDone (-1)
+  | `Invalid (_, e) -> raise e
 
 (* ----------------- Startup ---------------- *)
 