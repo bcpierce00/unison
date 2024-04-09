1394ec56e68f7af95eb8a6c5aa26a7172e46cd8a
Tõivo Leedjärv <toivol@gmail.com>
2024-04-09 20:50:57 +0200

    Change props of the temporary file for diffing
    For purposes of running a local diff, a remote file is copied by the
    same routines that are used fo the actual sync. This includes all checks
    and verifications. Using "safe" dummy props (as is currently done) to
    copy the remote file into a temporary file can in some cases trip those
    checks. This mainly happens when permissions are influenced by
    fs/storage setup or ACL inheritance, for example.
    
    This patch sets the temporary file props to be the same as what the
    local file already has. The hope is that setting these props would
    succeed during a normal sync, so it should also work for the temporary
    file.

--- src/files.ml
+++ src/files.ml
@@ -847,7 +847,7 @@ let rec diff root1 path1 ui1 root2 path2 ui2 showDiff id =
            Lwt_unix.run
              (Update.translatePath root2 path2 >>= (fun path2 ->
               Copy.file root2 path2 root1 workingDir tmppath realPath
-                `Copy (Props.setLength Props.fileSafe (Props.length desc2))
+                `Copy (Props.setLength desc1 (Props.length desc2))
                  fp2 None ress2 id) >>= fun info ->
               Lwt.return ());
            displayDiff
@@ -866,7 +866,7 @@ let rec diff root1 path1 ui1 root2 path2 ui2 showDiff id =
              (Update.translatePath root1 path1 >>= (fun path1 ->
               (* Note that we don't need the resource fork *)
               Copy.file root1 path1 root2 workingDir tmppath realPath
-                `Copy (Props.setLength Props.fileSafe (Props.length desc1))
+                `Copy (Props.setLength desc2 (Props.length desc1))
                  fp1 None ress1 id >>= fun info ->
               Lwt.return ()));
            displayDiff
